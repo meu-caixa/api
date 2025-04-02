5d32f916f1df101c75a60e28486218fca5521239
Kleyson Maciel <kleyson182@gmail.com>
2025-04-02 00:44:13 -0400

    feat(User): Criando model de usuário

---
+++ src/main/java/br/com/meucaixa/api/models/User.java
@@ -0,0 +1,35 @@
+package br.com.meucaixa.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String login;
+
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
