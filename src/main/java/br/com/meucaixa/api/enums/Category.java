1dd3d3d282f79e8539694f01fe51541b543e48f2
Kleyson Maciel <kleyson182@gmail.com>
2025-04-04 00:01:02 -0400

    feat(Enums): Criando Enums

---
+++ src/main/java/br/com/meucaixa/api/enums/Category.java
@@ -0,0 +1,6 @@
+package br.com.meucaixa.api.enums;
+
+public enum Category {
+    SALARIO,
+    RENDA_EXTRA
+}
---
+++ src/main/java/br/com/meucaixa/api/enums/TransactionType.java
@@ -0,0 +1,6 @@
+package br.com.meucaixa.api.enums;
+
+public enum TransactionType {
+    ENTRY,
+    EXPENSE
+}
