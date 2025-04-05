689748c9a53c4b492a45601052f423d4eef61c3b
roberto.aoki <nogueiraaoki@gmail.com>
2025-04-05 17:08:14 -0300

    fix: use record in controller dto

--- src/main/java/br/com/meucaixa/api/controllers/roles/RoleResponse.java
+++ src/main/java/br/com/meucaixa/api/controllers/roles/RoleResponse.java
@@ -1,12 +1,4 @@
 package br.com.meucaixa.api.controllers.roles;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class RoleResponse {
-    private int id;
-    private String name;
-    private String description;
+public record RoleResponse(int id, String name, String description) {
 }
